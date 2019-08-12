442af90742ec5dbb39921f92d2ff2eea7c66a8de
Euleoterio <46050068+Euleoterio@users.noreply.github.com>
2019-08-11 23:16:23 -0300

    Add files

---
+++ hello.py
@@ -0,0 +1,19 @@
+#My first program with python
+
+# -*- coding: utf-8 -*-
+print("Hello World!")
+print("Another message")
+print("Olá - com acento")
+
+print(2+2)
+print(2-2)
+print(2/2)
+print(2*2)
+print(2**3)
+print(10%3)
+
+
+"""
+many
+comments
+"""
---
+++ variaveis.py
@@ -0,0 +1,17 @@
+mensagem = "Hello World"
+
+print(mensagem)
+print(mensagem)
+print(mensagem)
+
+
+var1 = 1 #int
+var2 = 1.1 #float
+var3 = "String" #string
+var4 = True	#true
+var4 = False #false
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
